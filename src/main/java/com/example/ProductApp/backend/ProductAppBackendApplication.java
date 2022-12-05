d37524d029b72bd1dce0373520fd230c2e839f47
shanajasila <shanajasilam@gmail.com>
2022-12-05 17:25:13 +0530

    initial commit

---
+++ src/main/java/com/example/ProductApp/backend/ProductAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProductApp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/ProductApp/backend/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.example.ProductApp.backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class ProductController {
+
+}
